baca99b2f729d9d48d3970663207244ab0a5e92a
Pete O'Shea <mail@peteoshea.co.uk>
2020-09-03 17:47:51 +0100

    Add snapshot test to check renders same as before

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,8 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders as before', () => {
+  const { asFragment } = render(<App />);
+  expect(asFragment()).toMatchSnapshot();
+});
